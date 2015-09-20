cec891527b50a5a68bea31e27a7cb8a9f0526e17
Kay-Uwe Janssen <git@kujanssen.de>
2015-09-20 19:48:56 +0200

    allow to inject @EBean's into an @Bean annotated method

--- AndroidAnnotations/androidannotations-core/androidannotations/src/main/java/org/androidannotations/internal/core/handler/BeanHandler.java
+++ AndroidAnnotations/androidannotations-core/androidannotations/src/main/java/org/androidannotations/internal/core/handler/BeanHandler.java
@@ -15,9 +15,6 @@
  */
 package org.androidannotations.internal.core.handler;
 
-import static com.helger.jcodemodel.JExpr._null;
-import static com.helger.jcodemodel.JExpr.ref;
-
 import javax.lang.model.element.Element;
 import javax.lang.model.type.TypeMirror;
 
@@ -25,52 +22,53 @@ import org.androidannotations.AndroidAnnotationsEnvironment;
 import org.androidannotations.ElementValidation;
 import org.androidannotations.annotations.Bean;
 import org.androidannotations.annotations.EBean;
-import org.androidannotations.annotations.NonConfigurationInstance;
 import org.androidannotations.handler.BaseAnnotationHandler;
+import org.androidannotations.helper.InjectHelper;
 import org.androidannotations.holder.EBeanHolder;
 import org.androidannotations.holder.EComponentHolder;
+import org.androidannotations.holder.HasMethodInjection;
 
 import com.helger.jcodemodel.AbstractJClass;
+import com.helger.jcodemodel.IJExpression;
 import com.helger.jcodemodel.JBlock;
-import com.helger.jcodemodel.JFieldRef;
-import com.helger.jcodemodel.JInvocation;
 
-public class BeanHandler extends BaseAnnotationHandler<EComponentHolder> {
+public class BeanHandler extends BaseAnnotationHandler<EComponentHolder>implements HasMethodInjection<EComponentHolder> {
+
+	private final InjectHelper<EComponentHolder> injectHelper;
 
 	public BeanHandler(AndroidAnnotationsEnvironment environment) {
 		super(Bean.class, environment);
+		injectHelper = new InjectHelper<>(validatorHelper, this);
 	}
 
 	@Override
 	public void validate(Element element, ElementValidation validation) {
-		validatorHelper.enclosingElementHasEnhancedComponentAnnotation(element, validation);
-
-		validatorHelper.isNotPrivate(element, validation);
+		injectHelper.validate(Bean.class, element, validation);
 
 		validatorHelper.typeOrTargetValueHasAnnotation(EBean.class, element, validation);
+
+		validatorHelper.isNotPrivate(element, validation);
 	}
 
 	@Override
-	public void process(Element element, EComponentHolder holder) throws Exception {
+	public void process(Element element, EComponentHolder holder) {
+		injectHelper.process(element, holder);
+	}
+
+	@Override
+	public JBlock getInvocationBlock(EComponentHolder holder) {
+		return holder.getInitBody();
+	}
+
+	@Override
+	public IJExpression getInstanceInvocation(Element element, EComponentHolder holder, Element param) {
 		TypeMirror typeMirror = annotationHelper.extractAnnotationClassParameter(element);
 		if (typeMirror == null) {
-			typeMirror = element.asType();
+			typeMirror = param.asType();
 			typeMirror = getProcessingEnvironment().getTypeUtils().erasure(typeMirror);
 		}
-
 		String typeQualifiedName = typeMirror.toString();
 		AbstractJClass injectedClass = getJClass(annotationHelper.generatedClassQualifiedNameFromQualifiedName(typeQualifiedName));
-
-		String fieldName = element.getSimpleName().toString();
-		JFieldRef beanField = ref(fieldName);
-		JBlock block = holder.getInitBody();
-
-		boolean hasNonConfigurationInstanceAnnotation = element.getAnnotation(NonConfigurationInstance.class) != null;
-		if (hasNonConfigurationInstanceAnnotation) {
-			block = block._if(beanField.eq(_null()))._then();
-		}
-
-		JInvocation getInstance = injectedClass.staticInvoke(EBeanHolder.GET_INSTANCE_METHOD_NAME).arg(holder.getContextRef());
-		block.assign(beanField, getInstance);
+		return injectedClass.staticInvoke(EBeanHolder.GET_INSTANCE_METHOD_NAME).arg(holder.getContextRef());
 	}
 }
