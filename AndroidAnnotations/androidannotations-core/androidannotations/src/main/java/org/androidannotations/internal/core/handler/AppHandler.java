564b422394f88157b3ee0c610f15492b356c6377
Kay-Uwe Janssen <git@kujanssen.de>
2015-09-25 23:04:14 +0200

    allow to inject application class into @App annotated method

--- AndroidAnnotations/androidannotations-core/androidannotations/src/main/java/org/androidannotations/internal/core/handler/AppHandler.java
+++ AndroidAnnotations/androidannotations-core/androidannotations/src/main/java/org/androidannotations/internal/core/handler/AppHandler.java
@@ -15,7 +15,6 @@
  */
 package org.androidannotations.internal.core.handler;
 
-import static com.helger.jcodemodel.JExpr.ref;
 import static org.androidannotations.helper.ModelConstants.classSuffix;
 
 import javax.lang.model.element.Element;
@@ -25,32 +24,49 @@ import org.androidannotations.ElementValidation;
 import org.androidannotations.annotations.App;
 import org.androidannotations.annotations.EApplication;
 import org.androidannotations.handler.BaseAnnotationHandler;
+import org.androidannotations.helper.InjectHelper;
 import org.androidannotations.holder.EApplicationHolder;
 import org.androidannotations.holder.EComponentHolder;
+import org.androidannotations.holder.HasMethodInjection;
 
 import com.helger.jcodemodel.AbstractJClass;
+import com.helger.jcodemodel.IJExpression;
+import com.helger.jcodemodel.JBlock;
 
-public class AppHandler extends BaseAnnotationHandler<EComponentHolder> {
+public class AppHandler extends BaseAnnotationHandler<EComponentHolder>implements HasMethodInjection<EComponentHolder> {
+
+	private final InjectHelper<EComponentHolder> injectHelper;
 
 	public AppHandler(AndroidAnnotationsEnvironment environment) {
 		super(App.class, environment);
+		injectHelper = new InjectHelper<>(validatorHelper, this);
 	}
 
 	@Override
 	public void validate(Element element, ElementValidation validation) {
-		validatorHelper.enclosingElementHasEnhancedComponentAnnotation(element, validation);
-
-		validatorHelper.typeHasAnnotation(EApplication.class, element, validation);
+		injectHelper.validate(App.class, element, validation);
 
 		validatorHelper.isNotPrivate(element, validation);
+
+		Element param = injectHelper.getParam(element);
+		validatorHelper.typeHasAnnotation(EApplication.class, param, validation);
 	}
 
 	@Override
 	public void process(Element element, EComponentHolder holder) {
-		String fieldName = element.getSimpleName().toString();
-		String applicationQualifiedName = element.asType().toString();
+		injectHelper.process(element, holder);
+	}
+
+	@Override
+	public JBlock getInvocationBlock(EComponentHolder holder) {
+		return holder.getInitBody();
+	}
+
+	@Override
+	public IJExpression getInstanceInvocation(Element element, EComponentHolder holder, Element param) {
+		String applicationQualifiedName = param.asType().toString();
 		AbstractJClass applicationClass = getJClass(applicationQualifiedName + classSuffix());
 
-		holder.getInitBody().assign(ref(fieldName), applicationClass.staticInvoke(EApplicationHolder.GET_APPLICATION_INSTANCE));
+		return applicationClass.staticInvoke(EApplicationHolder.GET_APPLICATION_INSTANCE);
 	}
 }
